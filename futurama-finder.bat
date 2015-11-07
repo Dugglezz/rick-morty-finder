0fb0001813b35b59ea18f7aa5bbf41c43653d509
Douglas Hull <Douglas Hull>
2015-11-07 20:17:26 +0000

    added futurama finder

---
+++ futurama-finder.bat
@@ -0,0 +1,129 @@
+ @ECHO off
+ TITLE Futurama Episode Finder
+ :start
+ cls
+ ECHO.
+ ECHO Futurama
+ ECHO Series 1
+ ECHO 1.Space Pilot 3000
+ ECHO 2.The Series Has Landed
+ ECHO 3.I, Roommate
+ ECHO 4.Love's Labours Lost In Space
+ ECHO 5.Fear Of A Bot Planet
+ ECHO 6.A Fishfull Of Dollars
+ ECHO 7.My Three Suns
+ ECHO 8.A Big Piece Of Garbage
+ ECHO 9.Hell Is Other Robots
+ ECHO 10.A Flight To Remember
+ ECHO 11.Mars University
+ ECHO 12.When Aliens Attack
+ ECHO 13.Fry And The Slurm Factory
+ ECHO.
+ ECHO Series 2
+ ECHO 14.I Second That Emotion
+ ECHO 15.Brannigan, Begin Again
+ ECHO 16.A Head In The Polls
+ ECHO 17.Xmas Story
+ ECHO 18.Why Must I Be A Crustacean In Love
+ ECHO 19.The Lesser Of Two Evils
+ ECHO 20.Put Your Head On My Shoulders
+ ECHO 21.Raging Bender
+ ECHO 22.A Bicyclops Built For Two
+ ECHO 23.A Clone Of My Own
+ ECHO 24.How Hermes Requisitioned His Groove Back
+ ECHO 25.The Deep South
+ ECHO 26.Bender Gets Made
+ ECHO 27.Mother's Day
+ ECHO 28.The Problem With Popplers
+ ECHO 29.Anthology Of Interest I
+ ECHO 30.War Is The H-Word
+ ECHO 31.The Honking
+ ECHO 32.The Cryonic Woman
+ ECHO.
+ ECHO Series 3
+ ECHO 33.Amazon Women In The Mood
+ ECHO 34.Parasites Lost
+ ECHO 35.A Tale Of Two Santas
+ ECHO 36.Luck Of The Fryrish
+ ECHO 37.The Birdbot Of Ice-Catraz
+ ECHO 38.Bendless Love
+ ECHO 39.The Day The Earth Stood Stupid
+ ECHO 40.That's Lobstertainment!
+ ECHO 41.The Cyber House Rules
+ ECHO 42.Where The Buggalo Roam
+ ECHO 43.Insane In The Mainframe
+ ECHO 44.The Route Of All Evil
+ ECHO 45.Bendin' In The Wind
+ ECHO 46.Time Keeps On Slipping
+ ECHO 47.I Dated A Robot
+ ECHO 48.A Leela Of Her Own
+ ECHO 49.A Pharaoh To Remember
+ ECHO 50.Anthology Of Interest II
+ ECHO 51.Roswell That Ends Well
+ ECHO 52.Godfellas
+ ECHO 53.Futurestock
+ ECHO 54.30 Percent Iron Chef
+ ECHO.
+ set choice=
+ set /p choice=Enter episode number: 
+ REM Series 1
+ if '%choice%'=='1'  start "" https://2.bp.blogspot.com/zFpeINmemNeXv8kalRMDGW_ciHpSyyrh5CZ2BvCoT4k=m18
+ if '%choice%'=='2'  start "" https://2.bp.blogspot.com/Mj5UTS_xdFkuCefw29uTSPS8hq-sxI2Ynp0DJfr0y5E=m18
+ if '%choice%'=='3'  start "" https://2.bp.blogspot.com/JMcNUjkiTwttycWelX5ztlxQefmnyP5PYJ6KvQfvs-Y=m18
+ if '%choice%'=='4'  start "" https://2.bp.blogspot.com/GoNzRGXzf-42IgB_mopndfvMv48BxzaXCq_P2sTQtoc=m18
+ if '%choice%'=='5'  start "" https://2.bp.blogspot.com/2Y1Bq5tlxLVNp4jokk7PfnUKdpILuO8LXdfT2CvYtAM=m18
+ if '%choice%'=='6'  start "" https://2.bp.blogspot.com/jaGUg8L7aJz4OodWbKwJksAG8sxJBTcDoa3CaoZAvaU=m18
+ if '%choice%'=='7'  start "" https://2.bp.blogspot.com/ODrVWv8z5p_DYJL0ZCGVRPQqGTFRtlEvPr_fBt5yoYM=m18
+ if '%choice%'=='8'  start "" https://2.bp.blogspot.com/ArsAoBH0Onqs5Gh1XRGaT2T23O7_HhqAVCKutmGID9w=m18
+ if '%choice%'=='9'  start "" https://2.bp.blogspot.com/QWa95X6wodlitw9rC3ebTqIH9Qd-Bn6ebzIkNkbeb0w=m18
+ if '%choice%'=='10' start "" https://2.bp.blogspot.com/UvsCM7IDHvOA2bqIJSD_pxjiTFkulfd4SanjpcXZRfc=m18
+ if '%choice%'=='11' start "" https://2.bp.blogspot.com/NMrgXxxgArZP6ejpZ9TO2HMLIR60hoI3l54LxfePYLk=m18
+ if '%choice%'=='12' start "" https://2.bp.blogspot.com/eb_-OS0Fk910-pa1nAz5DX23uuHHZ7nxLghy_RwJOlw=m18
+ if '%choice%'=='13' start "" https://2.bp.blogspot.com/T3dbTgzt-hgMYEVQG3m4G4ICYPz016zqqc2pZJrq13s=m18
+ REM Series 2
+ if '%choice%'=='14' start "" https://2.bp.blogspot.com/atopwWL95V0K9QrjTHxwAK8-wuOwt375I8bBpGpbQ6Y=m18
+ if '%choice%'=='15' start "" https://2.bp.blogspot.com/nFjW4yShEh9tBvFpyLeN4roqzneAju3IltYh8BIPY1w=m18
+ if '%choice%'=='16' start "" https://2.bp.blogspot.com/7KloS1ztMovogMDXAyXHrJO7vktiyEaL8fwhsWWXijQ=m18
+ if '%choice%'=='17' start "" https://2.bp.blogspot.com/IQe2YGrqEBPnLG7osVWIsOW_OMIWBGBg10W4ot7pASM=m18
+ if '%choice%'=='18' start "" https://2.bp.blogspot.com/ErPZPLPOOJr4cXjnkuqAV1J1iTuhwsGzMBizRxdjXmo=m18
+ if '%choice%'=='19' start "" https://2.bp.blogspot.com/cc0EFTdl9n4FKRtD5j6UkbUlsqfUN3e6m0BIznqk0l4=m18
+ if '%choice%'=='20' start "" https://2.bp.blogspot.com/ZMlWAOAyfvuYfX9MSJIz-V8WT3rJml96wiI6AKFRGAY=m18
+ if '%choice%'=='21' start "" https://2.bp.blogspot.com/4UgEaDxNmzDiZC6wjeU6z6WvoW7GvveIMNxGiukGFNk=m18
+ if '%choice%'=='22' start "" https://2.bp.blogspot.com/3RB8gZBUWaRAMZRPa0ItoK-WjUWVFIlTruxLuXWUGu8=m18
+ if '%choice%'=='23' start "" https://2.bp.blogspot.com/Lw2GiSw0u4XMpEsJGupFD5VdrXYvZT6Z1OgzjTUBzlo=m18
+ if '%choice%'=='24' start "" https://2.bp.blogspot.com/wZ68IEV6KWSvLuBbfGr2xaRnIE12AJdGuA-zskSAJMc=m18
+ if '%choice%'=='25' start "" https://2.bp.blogspot.com/BlzsEqrfjWwc_SpEkSFpRtuBUdRbi320NhRGPfXrxwQ=m18
+ if '%choice%'=='26' start "" https://2.bp.blogspot.com/CXoYQ_mwdaZI8GKxOBmR8KZB7XUlLZ2gwVVEOOziVRA=m18
+ if '%choice%'=='27' start "" https://2.bp.blogspot.com/CaBIapiiwV3G_xbShW1TTGh1R-b_G3JlLXh8BGqz54A=m18
+ if '%choice%'=='28' start "" https://2.bp.blogspot.com/FQzzbuwLmEmTeOAWdgJu3HeQexy5PRJBQyXCL7Sz024=m18
+ if '%choice%'=='29' start "" https://2.bp.blogspot.com/cNO3_59kja7pxq-9F1DL8KWTVvmWUfmcGjTjUjJpHVI=m18
+ if '%choice%'=='30' start "" https://2.bp.blogspot.com/UQxmu8eUW03Wxo_ohk8wUP9-lqzUBWi3YphpY3qU8yI=m18
+ if '%choice%'=='31' start "" https://2.bp.blogspot.com/9sZQPqRLvRA9TQ7ni97mbGPryjgdR-KYdfsTtKizUsQ=m18
+ if '%choice%'=='32' start "" https://2.bp.blogspot.com/4A4tWu00DJKKP4g9BojEGgsvEJrzOHg6Z0V0bzOGL2I=m18
+ REM Series 3
+ if '%choice%'=='33' start "" https://2.bp.blogspot.com/WCDJrUq0g1pMABwGEXyCjDVyK8BnMSox7emOGwVP9Q=m22
+ if '%choice%'=='34' start "" https://2.bp.blogspot.com/H5mJxH5Pg7rTVuBHlRcrgfSW_ppXlLjCllDZwmUxXg=m22
+ if '%choice%'=='35' start "" https://2.bp.blogspot.com/HT_n-376C-gN1rLD7kWXC-0OArvgcGiNjjp20GRnDg=m22
+ if '%choice%'=='36' start "" https://2.bp.blogspot.com/pcNwpLvJQoygJTTr03-BuAXSuc2E9FaS2n4aToDybA=m22
+ if '%choice%'=='37' start "" https://2.bp.blogspot.com/lox-cT6lMGfUI5bukh94lWSmR0TPmFCLsS658Ed-Pw=m22
+ if '%choice%'=='38' start "" https://2.bp.blogspot.com/VJJs5xQbDApeb60o0UG5lQWRwibNlYtpYl9DGEOkCw=m22
+ if '%choice%'=='39' start "" https://2.bp.blogspot.com/wDKjAXPERbrIcSgSSSjA1IIAbmC62t_DcSFSHLfrMQ=m22
+ if '%choice%'=='40' start "" https://2.bp.blogspot.com/eU6qD4qHKbnnW_Tp7bPFWPNJ3VVP5u1zvfh6ZgCcqg=m22
+ if '%choice%'=='41' start "" https://2.bp.blogspot.com/O5Y3To76U2mEhfuiEgwXsqWaCLFnvfppqN2M-ue5CA=m22
+ if '%choice%'=='42' start "" https://2.bp.blogspot.com/84JTZunkpb_YU60lnREk2BL6v24kZLL4-WnXcqm7fg=m22
+ if '%choice%'=='43' start "" https://2.bp.blogspot.com/7J80Rumme4iTyJZ_t8dkemxaRfr9UysSF_8RDXD5xA=m22
+ if '%choice%'=='44' start "" https://2.bp.blogspot.com/Sa8Zrb6YwZgGZZIhTPFVshOAPdgX3ibMFVKUQ-Xrvw=m22
+ if '%choice%'=='45' start "" https://2.bp.blogspot.com/0npnPiKM0vUTiouedTf6-9wp-5OE9kQrdar68bEJGw=m22
+ if '%choice%'=='46' start "" https://2.bp.blogspot.com/yV2QvWaPuSgKeSHoWsArTU8v91ERFZjCw9AVJ-iD_sY=m22
+ if '%choice%'=='47' start "" https://2.bp.blogspot.com/At9ZGuFYiHezQnGXZx2aUdcQWOAVgQj_H5UatlT0bA=m22
+ if '%choice%'=='48' start "" https://2.bp.blogspot.com/FXfayrxlLNvISqoBYeHPIA3HHLfxpG1B6jKPc5qzCQ=m22
+ if '%choice%'=='49' start "" https://2.bp.blogspot.com/CZuh-lxUYufRJN-Zp4aaB8cQtScgTKNyT15SP2wAZQ=m22
+ if '%choice%'=='50' start "" https://2.bp.blogspot.com/1g7IlDgw9DFrv4MoDu0vOowOLhO49Oc4qwcYM7bn7B2a=m22
+ if '%choice%'=='51' start "" https://2.bp.blogspot.com/yjWq5ql4VL5FOF_4YEhtvczvvm2LOyMqfZ2pOZHM5Q=m22
+ if '%choice%'=='52' start "" https://2.bp.blogspot.com/_012VsSB6_0CrPt9L-sswSBFHG2vbE1orlCGfZIBEg=m22
+ if '%choice%'=='53' start "" https://2.bp.blogspot.com/17lFjagwfwKic3lKUgcLqwgFoYOHrK17aLrsZEKvUA=m22
+ if '%choice%'=='54' start "" https://2.bp.blogspot.com/AZFiLNBgyimLhUROdni8oYpHYLJZleN9D-qQ5fukOg=m22
+ PAUSE
+ goto start
+ :end
+ pause 
